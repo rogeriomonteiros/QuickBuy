5d51cb63abaad63ea75012fe934e89cff3258ecd
rogeriomonteiros <rogeriomonteiros@gmail.com>
2019-07-10 23:21:28 -0300

    Criacao do Projeto

---
+++ QuickyBuy.Dominio/Enum/TipoFormaPagamentoEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.Enum
+{
+    public enum TipoFormaPagamentoEnum
+    {
+        NaoDefinido = 0,
+        Boleto = 1,
+        CartaoDeCredito = 2,
+        Deposito = 3
+    }
+}
---
+++ QuickyBuy.Dominio/ObjetoDeValor/FormaPagamento.cs
@@ -0,0 +1,35 @@
+﻿using QuickBuy.Dominio.Enum;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.ObjetoDeValor
+{
+    public class FormaPagamento
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+
+        public bool EhBoleto
+        {
+            get { return (this.Id == (int)TipoFormaPagamentoEnum.Boleto); }
+        }
+
+        public bool EhCartaoCredito
+        {
+            get { return (this.Id == (int)TipoFormaPagamentoEnum.CartaoDeCredito); }
+        }
+
+        public bool EhDeposito
+        {
+            get { return (this.Id == (int)TipoFormaPagamentoEnum.Deposito); }
+        }
+
+        public bool EhNaoDefinido
+        {
+            get { return (this.Id == (int)TipoFormaPagamentoEnum.NaoDefinido); }
+        }
+
+    }
+}
