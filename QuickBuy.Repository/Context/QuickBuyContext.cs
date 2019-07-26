1735f076e61a5299abbe057475bf62fae9b84d6c
rogeriomonteiros <rogeriomonteiros@gmail.com>
2019-07-25 22:25:43 -0300

    Configurando o DbContext

---
+++ QuickBuy.Repository/Context/QuickBuyContext.cs
@@ -0,0 +1,24 @@
+﻿using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using QuickBuy.Dominio.Entidades;
+using QuickBuy.Dominio.ObjetoDeValor;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Repository.Context
+{
+    public class QuickBuyContext : DbContext
+    {
+        public QuickBuyContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<ItemPedido> ItemPedidos { get; set; }
+        public DbSet<FormaPagamento> FormaPagamento { get; set; }
+    }
+}
--- QuickBuy.Web/Startup.cs
+++ QuickBuy.Web/Startup.cs
@@ -3,8 +3,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using QuickBuy.Repository.Context;
 
 namespace QuickBuy.Web
 {
@@ -12,7 +14,9 @@ namespace QuickBuy.Web
     {
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            var builder = new ConfigurationBuilder();
+            builder.AddJsonFile("config.json",optional:false,reloadOnChange:true);
+            Configuration = builder.Build();
         }
 
         public IConfiguration Configuration { get; }
@@ -21,6 +25,9 @@ namespace QuickBuy.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            var connectionString = Configuration.GetConnectionString("MySqlConnection");
+            services.AddDbContext<QuickBuyContext>(option => option.UseMySql(connectionString,
+                                                            m => m.MigrationsAssembly("QuickBuy.Repository")));
 
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
